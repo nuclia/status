dbbf425d371cc7704596b711ea32fec1bd2d18ed
Carles Onielfa <carlesonielfa@gmail.com>
2025-08-19 16:02:35 +0200

    better

--- .github/scripts/validate_features.py
+++ .github/scripts/validate_features.py
@@ -1,7 +1,13 @@
+import sys
 import mrflagly
 from pathlib import Path
 
-FILE = "features-v2.json"
 
-file_text = Path(FILE).read_text()
-flag_service = mrflagly.FlagService(data=file_text)
+def main():
+    filename = sys.argv[1] if len(sys.argv) > 1 else "features-v2.json"
+    file_text = Path(filename).read_text()
+    mrflagly.FlagService(data=file_text)
+
+
+if __name__ == "__main__":
+    main()
